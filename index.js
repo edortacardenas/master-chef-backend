24f8e397b782b01c5e10cf80e3b4db9abeeac58b
Erick Dorta Cardenas <edortacardenas@gmail.com>
2025-07-18 09:27:39 -0400

    adding

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ dotenv.config(); //Load environment variables from .env file
 //Initialization express
 const app = express(); 
 
+console.log('La direccion del frontend es '+ process.env.FRONTEND_URL)
+
 //Configure cors to allow requests from localhost:3000
 app.use(cors({
     origin: process.env.FRONTEND_URL || "http://localhost:5173", //Allow requests from this origin
